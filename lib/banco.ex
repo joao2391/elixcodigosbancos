72804ce74632b90b0f1f67392211b0ba9a9c08a4
joao2391 <j_paulo_15@hotmail.com>
2022-03-22 06:16:06 -0300

    fix: applied mix format and mix credo

--- lib/banco.ex
+++ lib/banco.ex
@@ -1,3 +1,6 @@
 defmodule ElixCodigosBancos.Banco do
+  @moduledoc """
+  `ElixCodigosBancos.Banco` is a struct to organize the results.
+  """
   defstruct codigo: "", nome: "", site: ""
 end
--- lib/elixcodigosbancos.ex
+++ lib/elixcodigosbancos.ex
@@ -1,6 +1,6 @@
 defmodule ElixCodigosBancos do
   @moduledoc """
-  Documentation for `ElixCodigosBancos`.
+  `ElixCodigosBancos` is a lib that helps you to get brazilian's offset codes.
   """
 
   require Logger
@@ -11,18 +11,27 @@ defmodule ElixCodigosBancos do
 
   """
   def get_banco_by_codigo(codigo) when is_bitstring(codigo) do
-    case HTTPoison.get("https://www.codigobanco.com/",
-                        %{"User-Agent" => "elixcodigosbancos/1.0.1"}) do
-      {:ok, %{body: raw_body, status_code: _code}} -> html = raw_body
+    case HTTPoison.get(
+           "https://www.codigobanco.com/",
+           %{"User-Agent" => "elixcodigosbancos/1.0.1"}
+         ) do
+      {:ok, %{body: raw_body, status_code: _code}} ->
+        html = raw_body
 
-      {:ok, document} = Floki.parse_document(html)
+        {:ok, document} = Floki.parse_document(html)
 
-      banco =
-        document
-        |> Floki.find("tbody")
-        |> Floki.find("tr")
-        |> Enum.map(fn {_chave, _valor1, valor2} -> %Banco{codigo: Enum.at(valor2,0) |> Floki.text(), nome: Enum.at(valor2,1) |> Floki.text(), site: Enum.at(valor2,2) |> Floki.text()} end)
-        |> Enum.filter(fn (x) -> x.codigo == codigo end)
+        banco =
+          document
+          |> Floki.find("tbody")
+          |> Floki.find("tr")
+          |> Enum.map(fn {_chave, _valor1, valor2} ->
+            %Banco{
+              codigo: Enum.at(valor2, 0) |> Floki.text(),
+              nome: Enum.at(valor2, 1) |> Floki.text(),
+              site: Enum.at(valor2, 2) |> Floki.text()
+            }
+          end)
+          |> Enum.filter(fn x -> x.codigo == codigo end)
 
         banco
 
@@ -35,17 +44,26 @@ defmodule ElixCodigosBancos do
   Busca todos os bancos
   """
   def get_bancos() do
-    case HTTPoison.get("https://www.codigobanco.com/",
-                        %{"User-Agent" => "elixcodigosbancos/1.0.1"}) do
-      {:ok, %{body: raw_body, status_code: _code}} -> html = raw_body
+    case HTTPoison.get(
+           "https://www.codigobanco.com/",
+           %{"User-Agent" => "elixcodigosbancos/1.0.1"}
+         ) do
+      {:ok, %{body: raw_body, status_code: _code}} ->
+        html = raw_body
 
-      {:ok, document} = Floki.parse_document(html)
+        {:ok, document} = Floki.parse_document(html)
 
-      bancos =
-        document
-        |> Floki.find("tbody")
-        |> Floki.find("tr")
-        |> Enum.map(fn {_chave, _valor1, valor2} -> %Banco{codigo: Enum.at(valor2,0) |> Floki.text(), nome: Enum.at(valor2,1) |> Floki.text(), site: Enum.at(valor2,2) |> Floki.text()} end)
+        bancos =
+          document
+          |> Floki.find("tbody")
+          |> Floki.find("tr")
+          |> Enum.map(fn {_chave, _valor1, valor2} ->
+            %Banco{
+              codigo: Enum.at(valor2, 0) |> Floki.text(),
+              nome: Enum.at(valor2, 1) |> Floki.text(),
+              site: Enum.at(valor2, 2) |> Floki.text()
+            }
+          end)
 
         bancos
 
@@ -54,22 +72,31 @@ defmodule ElixCodigosBancos do
     end
   end
 
-   @doc """
+  @doc """
   Busca as informacoes do banco de acordo com o nome
   """
   def get_banco_by_nome(nome) when is_bitstring(nome) do
-    case HTTPoison.get("https://www.codigobanco.com/",
-                        %{"User-Agent" => "elixcodigosbancos/1.0.1"}) do
-      {:ok, %{body: raw_body, status_code: _code}} -> html = raw_body
+    case HTTPoison.get(
+           "https://www.codigobanco.com/",
+           %{"User-Agent" => "elixcodigosbancos/1.0.1"}
+         ) do
+      {:ok, %{body: raw_body, status_code: _code}} ->
+        html = raw_body
 
-      {:ok, document} = Floki.parse_document(html)
+        {:ok, document} = Floki.parse_document(html)
 
-      banco =
-        document
-        |> Floki.find("tbody")
-        |> Floki.find("tr")
-        |> Enum.map(fn {_chave, _valor1, valor2} -> %Banco{nome: Enum.at(valor2,1) |> Floki.text(), codigo: Enum.at(valor2,0) |> Floki.text(), site: Enum.at(valor2,2) |> Floki.text()} end)
-        |> Enum.filter(fn (x) -> String.contains?(x.nome, nome) end)
+        banco =
+          document
+          |> Floki.find("tbody")
+          |> Floki.find("tr")
+          |> Enum.map(fn {_chave, _valor1, valor2} ->
+            %Banco{
+              nome: Enum.at(valor2, 1) |> Floki.text(),
+              codigo: Enum.at(valor2, 0) |> Floki.text(),
+              site: Enum.at(valor2, 2) |> Floki.text()
+            }
+          end)
+          |> Enum.filter(fn x -> String.contains?(x.nome, nome) end)
 
         banco
 
@@ -77,7 +104,4 @@ defmodule ElixCodigosBancos do
         Logger.error(inspect(reason))
     end
   end
-
-
-
 end
